71b9173d4de5777761a0ee03a352b2bdbd975132
Luis Alberto Torres Gutierrez <luchoguti9@gmail.com>
2023-04-03 04:11:02 -0500

    Implementación method payment.

--- src/components/CanvasDetailsGeneral.jsx
+++ src/components/CanvasDetailsGeneral.jsx
@@ -2,6 +2,7 @@ import {useSelector} from "react-redux";
 import ProductDetail from "./ProductDetail";
 import {CanvasDetailsGeneralStyle} from "../styles_components/CanvasDetailsGeneralStyle";
 import StoreListProduct from "./StoreListProduct";
+import React from "react";
 
 const CanvasDetailsGeneral = () => {
     const {
@@ -21,6 +22,11 @@ const CanvasDetailsGeneral = () => {
             {
                 Object.keys(storeActions.detail_product).length === 0 && storeActions.view_store_detail && (
                     <StoreListProduct/>
+                )
+            }
+            {
+                Object.keys(storeActions.detail_product).length === 0 && !storeActions.view_store_detail  && (
+                    <p className="empty_label">Please choose a product on the left.</p>
                 )
             }
         </CanvasDetailsGeneralStyle>
--- src/styles_components/CanvasDetailsGeneralStyle.jsx
+++ src/styles_components/CanvasDetailsGeneralStyle.jsx
@@ -3,7 +3,14 @@ import {device_max_with} from "./MediaQuerys";
 
 export const CanvasDetailsGeneralStyle = styled.div`
   width: 50%;
+  ${this} .empty_label{
+    color: gray;
+    font-size: 19px;
+  }
   @media (${device_max_with.tablet}) {
     font-size: 9px;
+    ${this} .empty_label{
+      font-size: 10px;
+    }
   }
 `
